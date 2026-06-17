fc0007e2808db54d652464fc3a29057948deddb1
LerfVal <nooblee1255@gmail.com>
2026-06-16 22:11:45 -0400

    Add yml files
    added files for docker

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  output: "standalone", // required for the Docker runner stage
 };
 
 export default nextConfig;
